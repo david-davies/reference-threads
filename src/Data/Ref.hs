8bb5b85539c4a8040c5c61b22f21edb41398a4a3
Jamie Willis <j.willis19@imperial.ac.uk>
2024-04-05 15:39:53 +0100

    adjusted roles on Ref

--- src/Data/Ref.hs
+++ src/Data/Ref.hs
@@ -4,12 +4,22 @@ module Data.Ref (module Data.Ref) where
 
 import Control.Monad.RT (RT(RT))
 
-import GHC.Base (MutVar#, RealWorld, newMutVar#, readMutVar#, writeMutVar#)
+import GHC.Base (MutVar#, RealWorld, newMutVar#, readMutVar#, writeMutVar#, sameMutVar#, isTrue#)
 
 import GHC.IORef (IORef(IORef))
 import GHC.STRef (STRef(STRef))
 
-type role Ref phantom representational
+{-
+The `r` parameter is deliberately nominal here, which prevents the use of
+coerce to "safetly" escape the lifetime of a reference:
+
+> escape :: a -> RT (Ref r a)
+> escape x = newRef x (return . coerce)
+
+Would compile if the annotation was left as phantom (which is the inferred).
+This is clearly not something we want to allow, so nominal it is.
+-}
+type role Ref nominal representational
 -- Don't even expose the constructor, then it's pretty much safe?
 data Ref r a = Ref (MutVar# RealWorld a)
 
@@ -33,3 +43,7 @@ writeRef (Ref ref#) x = RT $ \s# ->
 {-# INLINE fromIORef #-}
 fromIORef :: IORef a -> Ref r a
 fromIORef (IORef (STRef ref#)) = Ref ref#
+
+-- It's nice that two references must clearly have the same lifetime to be equal
+instance Eq (Ref r a) where
+  Ref ref1# == Ref ref2# = isTrue# (sameMutVar# ref1# ref2#)
